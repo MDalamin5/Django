ef8b69af4d72820d86ec8b60597d062106e71214
MDalamin5 <mdal.amin5@northsouth.edu>
2023-09-23 11:37:11 +0600

    static html and css file add in django

--- third_project/first_app/views.py
+++ third_project/first_app/views.py
@@ -3,4 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 
 def contact(request):
-    return render(request, './first_app/index.html' , context= {'author': 'Md Al Amin', 'age':24,'mark': 79})
+    return render(request, './first_app/index.html' , {"name" : "Md Al Amin", "marks" : 86, "lst" : [24,3, 10, 5], "blog" : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", "courses" : [
+        {
+            'id' : 1,
+        'course' : 'C',
+        'teacher' : 'Al Amin'
+        },
+        {
+            
+            'id' : 2,
+        'course' : 'Cpp',
+        'teacher' : 'Kaysar'
+        },
+        {
+            
+            'id' : 3,
+        'course' : 'Django',
+        'teacher' : 'Phitron'
+        },
+    ]} )
+
+
