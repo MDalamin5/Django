00008c5d4a3db6d786be8e90c4415af226ed195c
MDalamin5 <mdal.amin5@northsouth.edu>
2023-12-06 01:17:46 +0600

    url tag in Django

---
+++ Week-1/Module-3/fourth_project/first_app/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def contact(request):
+    return render(request, 'f_app/index.html', {'name' : 'I am Rahim', 'marks' : 86,
+                                                'courses' : [
+        {
+            'id' : 1,
+            'course' : 'C',
+            'teacher' : 'Rahim'
+        },
+        {
+            'id' : 2,
+            'course' : 'C++',
+            'teacher' : 'KhaRahim'
+        },
+        {
+            'id' : 3,
+            'course' : 'Python',
+            'teacher' : 'Fahim'
+        }
+    ]})
+
+def about(request):
+    return render(request, 'f_app/about.html')
