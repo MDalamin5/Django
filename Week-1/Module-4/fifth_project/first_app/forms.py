d645a2f18323ce2e9368ec6e653a35d42a7c6503
MDalamin5 <mdal.amin5@northsouth.edu>
2023-12-09 00:32:21 +0600

    django form practice

--- Week-1/Module-4/fifth_project/first_app/forms.py
+++ Week-1/Module-4/fifth_project/first_app/forms.py
@@ -2,4 +2,15 @@ from django import forms
 
 class contactForm(forms.Form):
     name = forms.CharField(label="User name")
+    file = forms.FileField(label='Profile Picture')
     email = forms.EmailField(label= 'User email')
+    age = forms.IntegerField(label="Age")
+    weight = forms.FloatField(label='Weight')
+    balance = forms.DecimalField(label='User Balance')
+    check = forms.BooleanField(label='Single')
+    birthday = forms.DateField(label="Birthday")
+    appointment = forms.DateTimeField(label='Date Time for appointment')
+    CHOICE = [('S', 'Small'), ('M', 'Medium'), ('L', 'Large')]
+    siez = forms.ChoiceField(choices=CHOICE)
+    MEAL = [('P', 'Pepperoni'), ('B', 'Beef'), ('M', 'Mashroom')]
+    pizza = forms.MultipleChoiceField(choices=MEAL)
--- Week-1/Module-4/fifth_project/first_app/views.py
+++ Week-1/Module-4/fifth_project/first_app/views.py
@@ -23,7 +23,15 @@ def submit_form(request):
 
 
 def django_form(request):
-    form = contactForm(request.POST)
-    if form.is_valid():
-        print(form.cleaned_data)
+    if request.method == 'POST':
+        form = contactForm(request.POST, request.FILES)
+        if form.is_valid():
+            file = form.cleaned_data['file']
+            with open('./first_app/upload/' + file.name, 'wb+') as destination:
+                for chunk in file.chunks():
+                    destination.write(chunk)
+            print(form.cleaned_data)
+            return render(request, 'f_app/django_form.html', {'form' : form})
+    else:
+        form = contactForm()
     return render(request, 'f_app/django_form.html', {'form' : form})
