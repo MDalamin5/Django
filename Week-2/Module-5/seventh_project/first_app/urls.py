87f59ca0c054a1e4bdf6321fd0155272b271188e
MDalamin5 <mdal.amin5@northsouth.edu>
2023-12-12 18:32:43 +0600

    extract data from database and showing in consol

--- Week-2/Module-5/seventh_project/first_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from first_app.views import home, show_data
-
-urlpatterns = [
-    # path('', views.home, name="homepage"),
-    path('', home, name="homepage"),
-    path('show_data/', show_data, name="showData")
-]
