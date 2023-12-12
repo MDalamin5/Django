be50c061930aa56f2454b75cfa435faef6cfe349
MDalamin5 <mdal.amin5@northsouth.edu>
2023-12-12 12:58:55 +0600

    django abstractuion , inharitance, proxy model done

--- Week-2/Module-5/seventh_project/first_app/admin.py
+++ Week-2/Module-5/seventh_project/first_app/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
-from first_app.models import StudentModel
+from first_app.models import StudentModel, StudentInfoModel, TeacherInfoModel, EmployeeModel, ManagerModel, FriendModel, MeModel
 # Register your models here.
 
 admin.site.register(StudentModel)
+# admin.site.register(StudentInfoModel)
+# admin.site.register(TeacherInfoModel)
+# admin.site.register(EmployeeModel)
+# admin.site.register(ManagerModel)
+
+@admin.register(EmployeeModel)
+class EmployeeModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'city', 'designation']
+
+
+@admin.register(ManagerModel)
+class ManagerModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'city', 'designation', 'take_interview', 'hiring']
+
+
+@admin.register(FriendModel)
+class FriendModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'school', 'section', 'attendence', 'home_work']
+
+
+
+@admin.register(MeModel)
+class MeModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'school', 'section', 'attendence', 'home_work']
+
 
--- Week-2/Module-5/seventh_project/first_app/models.py
+++ Week-2/Module-5/seventh_project/first_app/models.py
@@ -10,3 +10,56 @@ class StudentModel(models.Model):
 
     def __str__(self) -> str:
         return f"Roll: {self.roll}  Name: {self.name}"
+
+
+"""
+        1. Model Inharetance
+        2. Multitable Inharitance
+        3. proxy Model
+
+        1. abstract base class
+"""
+ # abstract Base Class
+class CommonInfoClass(models.Model):
+    name = models.CharField(max_length=50)
+    city = models.CharField(max_length=40)
+    class Meta:
+        abstract = True
+
+class StudentInfoModel(CommonInfoClass):
+    roll = models.IntegerField(null=True)
+    payment = models.IntegerField()
+    section = models.CharField(max_length=70)
+
+
+
+class TeacherInfoModel(CommonInfoClass):
+    salary = models.IntegerField()
+
+
+# multitable Inharitance
+
+class EmployeeModel(models.Model):
+    name = models.CharField(max_length=70)
+    city = models.CharField(max_length=50)
+    designation = models.CharField(max_length=100)
+
+class ManagerModel(EmployeeModel):
+    take_interview = models.BooleanField()
+    hiring = models.BooleanField()
+
+
+#3 Proxy Model
+
+class FriendModel(models.Model):
+    school = models.CharField(max_length=50)
+    section = models.CharField(max_length=10)
+    attendence = models.BooleanField()
+    home_work = models.CharField(max_length=500)
+    
+
+class MeModel(FriendModel):
+    class Meta:
+        proxy = True
+
+        
