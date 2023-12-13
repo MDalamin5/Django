c45eae8820e422c1c20eb9a6ac496e4f21231d4b
MDalamin5 <mdal.amin5@northsouth.edu>
2023-12-13 12:20:42 +0600

    user cration from frontend

--- Week-2/Module-8/eight_project/first_app/views.py
+++ Week-2/Module-8/eight_project/first_app/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from . forms import RegisterForm
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
-    form = RegisterForm()
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            messages.success(request, 'Account Created Successfully')
+            # messages.warning(request, 'Warning')
+            # messages.info(request, 'info')
+            print(form.cleaned_data)
+            form.save()
+    else:
+        form = RegisterForm()
     return render(request, 'index.html', {'form' : form})
