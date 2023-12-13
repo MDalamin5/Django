5b4fd0768f0bd9810417c4a0a0c8e02092e58c9d
MDalamin5 <mdal.amin5@northsouth.edu>
2023-12-13 13:16:24 +0600

    login system buildup

--- Week-2/Module-8/eight_project/first_app/urls.py
+++ Week-2/Module-8/eight_project/first_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from first_app.views import home
+from first_app.views import home, signUp, profile
 
 urlpatterns = [
-    path('',home, name='homepage')
+    path('',home, name='homepage'),
+    path('signup/', signUp, name="signup"),
+    path('profile/', profile, name="profile_page"),
 ]
--- Week-2/Module-8/eight_project/first_app/views.py
+++ Week-2/Module-8/eight_project/first_app/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . forms import RegisterForm
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
 def home(request):
+    return render(request, 'home.html')
+
+def signUp(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
@@ -15,4 +20,24 @@ def home(request):
             form.save()
     else:
         form = RegisterForm()
-    return render(request, 'index.html', {'form' : form})
+    return render(request, './signup.html', {'form' : form})
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data = request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['username']
+            userpass = form.cleaned_data['password']
+            user = authenticate(username = name, password = userpass) # to check user valid or Not and user exist in databse or not
+            if user is not None:
+                login(request, user)
+                return redirect('profile_page')
+    else:
+        form = AuthenticationForm()
+        return render(request, 'login.html', {'form' : form})
+            
+
+
+def profile(request):
+    return render(request, 'profile.html')
+
