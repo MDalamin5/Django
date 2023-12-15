cca85e163d79fce00e681386459a28c43764f8c2
MDalamin5 <mdal.amin5@northsouth.edu>
2023-12-15 20:53:24 +0600

    Done Web cookie police

--- Week-3/Module-9/nineth_project/first_app/urls.py
+++ Week-3/Module-9/nineth_project/first_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home, name="homepage"),
-    path('get/', views.get_cooki),
-    path('del/', views.del_cooki),
+    # path('', views.home, name="homepage"),
+    path('', views.set_session, name="homepage"),
+    # path('get/', views.get_cooki),
+    path('get/', views.get_session),
+    # path('del/', views.del_cooki),
+    path('del/', views.delete_Session),
 ]
--- Week-3/Module-9/nineth_project/first_app/views.py
+++ Week-3/Module-9/nineth_project/first_app/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
+from datetime import datetime, timedelta
+from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
     response = render(request, 'home.html')
-    response.set_cookie('name', 'Md Al Amin')
-    response.set_cookie('Uni', 'NSU')
+    response.set_cookie('name', 'Md Al Amin', expires=datetime.utcnow()+timedelta(days=7))
     return response
 
 def get_cooki(request):
@@ -15,5 +16,51 @@ def get_cooki(request):
 
 
 def del_cooki(request):
-    pass
+    resposne = render(request, 'del.html')
+    resposne.delete_cookie('name')
+    return resposne
+
+# session vs cooki
+# cooki one kind of clint database or user or visitor data base his browser
+# session is the website data base where user importent data save in web-site main database
+# session for authentication
+# cooki: in our website what you want to see in our website, remmber me ai doranr jonno
+
+
+def set_session(request):
+    # data = {
+    #     'name' : 'Md Al Amin', 
+    #     'age' : 23,
+    #     'language' : 'Bangggggla'
+    # }
+    # print(request.session.get_session_cookie_age())
+    # print(request.session.get_expiry_date())
+
+    # request.session.update(data)
+    request.session['name'] = 'Al Amin'
+    return render(request, 'home.html')
+
+
+# this function is no same as the course instructior
+def get_session(request):
+    if 'name' in request.session:
+        data = request.session
+        request.session.modified = True
+        return render(request, 'get.html', {'data' : data})
+    else:
+        return HttpResponse("Your session is expired. Login againg")
+
+
+# instructor function
+
+# def get_seesion(request):
+#     name = request.session.get('name')
+#     age = request.session.get('age')
+#     language = request.session.get('language')
+#     return render(request, 'get.html', {'name': name, 'age' : age})
+
+
+def delete_Session(request):
+    request.session.flush()
+    return render(request, 'del.html')
 
