c079da5a5de9e44bf8a837483462405e0d7c0a39
MDalamin5 <mdal.amin5@northsouth.edu>
2023-12-26 00:42:45 +0600

    CBV -- DetailView

---
+++ Random-Practice/Class-Based-VIEWs/Django-CBV/DetailView/books/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Books(models.Model):
+    # Example model only
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(null=True)
+    genre = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=100)
+    count = models.IntegerField(null=True, default=0)
+    
+    def __str__(self) -> str:
+        return f"{self.title} ---- {self.isbn}"
